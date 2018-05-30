88ce900386813907180c9732e832b4921147c40a
Prachi Gupta <prachi.gupta@irissoftware.com>
2018-05-30 14:52:40 +0530

    Commit-4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-
-import { EventProposalComponent } from './Pre-Event Services/event-proposal/event-proposal.component';
 import { RouterModule,Routes } from '@angular/router';
-import { ReceivedContributionComponent } from './Pre-Event Services/received-contribution/received-contribution.component';
+
+import { AppComponent } from './app.component';
 import { TeamMemberDetailsComponent } from './Home/team-member-details/team-member-details.component';
+import { EventProposalComponent } from './Pre-Event Services/event-proposal/event-proposal.component';
+import { ReceivedContributionComponent } from './Pre-Event Services/received-contribution/received-contribution.component';
 import { CelebrationSummaryComponent } from './Post-Event Services/celebration-summary/celebration-summary.component';
 import { TeamMemberSummaryComponent } from './Post-Event Services/team-member-summary/team-member-summary.component';
 import { CelebrationExpensesComponent } from './Post-Event Services/celebration-expenses/celebration-expenses.component';
