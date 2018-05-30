f43daabffd7647997760d709f72b018ee3dfb98c
Aishwarya Gupta <aishwarya.gupta@irissoftware.com>
2018-05-30 14:28:13 +0530

    checked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
-import { EventProposalComponent } from './Pre-Event Services/event-proposal.component';
-import { ReceivedContributionComponent } from './Pre-Event Services/received-contribution/received-contribution.component';
+
 
 import { EventProposalComponent } from './Pre-Event Services/event-proposal/event-proposal.component';
 import { RouterModule,Routes } from '@angular/router';
 import { ReceivedContributionComponent } from './Pre-Event Services/received-contribution/received-contribution.component';
-import { TeamMemberDetailsComponent } from './Home/team-member-details/team-member-details.component';
-import { CelebrationComponent } from './Post-Event Services/celebration/celebration.component';
-import { TeamComponent } from './Post-Event Services/team/team.component';
+
 import { CelebrationSummaryComponent } from './Post-Event Services/celebration-summary/celebration-summary.component';
 import { TeamMemberSummaryComponent } from './Post-Event Services/team-member-summary/team-member-summary.component';
 import { CelebrationExpensesComponent } from './Post-Event Services/celebration-expenses/celebration-expenses.component';
@@ -30,8 +27,11 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     EventProposalComponent,
-    ReceivedContributionComponent
-
+    ReceivedContributionComponent,
+    CelebrationSummaryComponent,
+    TeamMemberSummaryComponent,
+    CelebrationExpensesComponent
+  ],
   
   imports: [
     BrowserModule,
@@ -39,6 +39,6 @@ const appRoutes: Routes = [
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponentTeamMemberDetailsComponentCelebrationComponentTeamComponentCelebrationSummaryComponentTeamMemberSummaryComponentCelebrationExpensesComponent]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
