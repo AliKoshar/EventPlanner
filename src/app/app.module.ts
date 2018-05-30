5f6224b3ce18ab3de4623c19bd173f3d288c0d57
Prachi Gupta <prachi.gupta@irissoftware.com>
2018-05-30 11:45:57 +0530

    module file commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,41 +4,52 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
-import { EventProposalComponent } from './Pre-Event Services/event-proposal.component';
-import { ReceivedContributionComponent } from './Pre-Event Services/received-contribution/received-contribution.component';
-
 import { EventProposalComponent } from './Pre-Event Services/event-proposal/event-proposal.component';
 import { RouterModule,Routes } from '@angular/router';
 import { ReceivedContributionComponent } from './Pre-Event Services/received-contribution/received-contribution.component';
 import { TeamMemberDetailsComponent } from './Home/team-member-details/team-member-details.component';
-import { CelebrationComponent } from './Post-Event Services/celebration/celebration.component';
-import { TeamComponent } from './Post-Event Services/team/team.component';
 import { CelebrationSummaryComponent } from './Post-Event Services/celebration-summary/celebration-summary.component';
 import { TeamMemberSummaryComponent } from './Post-Event Services/team-member-summary/team-member-summary.component';
 import { CelebrationExpensesComponent } from './Post-Event Services/celebration-expenses/celebration-expenses.component';
 
 
-
 const appRoutes: Routes = [
-  { path: 'eventproposal', component: EventProposalComponent }
- 
-  
+  { path: 'teammemberdetails', component: TeamMemberDetailsComponent }, 
+  { path: 'eventproposal', component: EventProposalComponent },
+  { path: 'receivedcontribution', component: ReceivedContributionComponent },
+  { path: 'celebrationsummary', component: CelebrationSummaryComponent },
+  { path: 'teammembersummary', component: TeamMemberSummaryComponent },
+  { path: 'celebrationexpenses', component: CelebrationExpensesComponent }
 ];
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    TeamMemberDetailsComponent,
     EventProposalComponent,
-    ReceivedContributionComponent
-
+    ReceivedContributionComponent,
+    CelebrationSummaryComponent,
+    TeamMemberSummaryComponent,
+    CelebrationExpensesComponent
+  ],
   
   imports: [
     BrowserModule,
     FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
+
   providers: [],
-  bootstrap: [AppComponentTeamMemberDetailsComponentCelebrationComponentTeamComponentCelebrationSummaryComponentTeamMemberSummaryComponentCelebrationExpensesComponent]
+
+  bootstrap: [
+    AppComponent,
+    TeamMemberDetailsComponent,
+    EventProposalComponent,
+    ReceivedContributionComponent,
+    CelebrationSummaryComponent,
+    TeamMemberSummaryComponent,
+    CelebrationExpensesComponent
+  ]
 })
 export class AppModule { }
