0c980ec1640b73ea8372bb92be239239ab37dbce
Aishwarya Gupta <aishwarya.gupta@irissoftware.com>
2018-05-30 10:49:38 +0530

    Checked event proposal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { EventProposalComponent } from './Pre-Event Services/event-proposal.component';
+import { EventProposalComponent } from './Pre-Event Services/event-proposal/event-proposal.component';
+import { RouterModule,Routes } from '@angular/router';
+
+
+
+const appRoutes: Routes = [
+  { path: 'eventproposal', component: EventProposalComponent }
+ 
+  
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     EventProposalComponent
+  
   ],
+  
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
