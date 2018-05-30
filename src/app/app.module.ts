0349d7b75076c81eed01fa9dad68660ef60c65f6
Aishwarya Gupta <aishwarya.gupta@irissoftware.com>
2018-05-30 14:49:00 +0530

    checked

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-
-
-
-import { EventProposalComponent } from './Pre-Event Services/event-proposal/event-proposal.component';
 import { RouterModule,Routes } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { TeamMemberDetailsComponent } from './Home/team-member-details/team-member-details.component';
+import { EventProposalComponent } from './Pre-Event Services/event-proposal/event-proposal.component';
 import { ReceivedContributionComponent } from './Pre-Event Services/received-contribution/received-contribution.component';
-
 import { CelebrationSummaryComponent } from './Post-Event Services/celebration-summary/celebration-summary.component';
 import { TeamMemberSummaryComponent } from './Post-Event Services/team-member-summary/team-member-summary.component';
 import { CelebrationExpensesComponent } from './Post-Event Services/celebration-expenses/celebration-expenses.component';
 
 
 const appRoutes: Routes = [
-  
+  { path: 'teammemberdetails', component: TeamMemberDetailsComponent }, 
   { path: 'eventproposal', component: EventProposalComponent },
   { path: 'receivedcontribution', component: ReceivedContributionComponent },
   { path: 'celebrationsummary', component: CelebrationSummaryComponent },
@@ -29,7 +26,7 @@ const appRoutes: Routes = [
 @NgModule({
   declarations: [
     AppComponent,
-   
+    TeamMemberDetailsComponent,
     EventProposalComponent,
     ReceivedContributionComponent,
     CelebrationSummaryComponent,
@@ -44,8 +41,15 @@ const appRoutes: Routes = [
   ],
 
   providers: [],
-  bootstrap: [AppComponent]
 
- 
+  bootstrap: [
+    AppComponent,
+    TeamMemberDetailsComponent,
+    EventProposalComponent,
+    ReceivedContributionComponent,
+    CelebrationSummaryComponent,
+    TeamMemberSummaryComponent,
+    CelebrationExpensesComponent
+  ]
 })
 export class AppModule { }
